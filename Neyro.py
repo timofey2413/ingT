5a96ff55c77dbd8fd8bc75dc2a80da1c3ed71c90
GigaProgramm <167605617+GigaProgramm@users.noreply.github.com>
2024-09-21 20:21:18 +0300

    нейро

--- Neyro.py
+++ Neyro.py
@@ -4,7 +4,9 @@ import cv2
 import os
 # получаем путь к этому скрипту
 import io
-
+import sqlite3
+conn = sqlite3.connect('DataBase.db')
+cursor = conn.cursor()
 path = os.path.dirname(os.path.abspath(__file__))
 # создаём новый распознаватель лиц
 recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -26,10 +28,12 @@ while True:
     gray=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     # определяем лица на видео
     faces=faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(100, 100), flags=cv2.CASCADE_SCALE_IMAGE)
-    # перебираем все найденные лица
+    # перебираем все найденные лица\
+    
     for(x,y,w,h) in faces:
         # получаем id пользователя
         nbr_predicted,coord = recognizer.predict(gray[y:y+h,x:x+w])
+
         # рисуем прямоугольник вокруг лица
         cv2.rectangle(im,(x-50,y-50),(x+w+50,y+h+50),(225,0,0),2)
         # если мы знаем id пользователя
